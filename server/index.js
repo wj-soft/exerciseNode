cf3fef85d9fb6dcc5ba36f7a14dbc8a680dbe7c3
wj-soft <wooju_lee@daum.net>
2018-03-27 14:19:03 +0900

    server routing

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 
 var express = require('express');
 
+var board = require('./routes/board');
+
 var app = express();
 var port = process.env.PORT || 4500;
 
 app.get('/', function(req,res){
   res.send('hello world')
 });
+
+//ROUTING
+app.use('/board', board)
 
 app.listen(port, function(req,res){
   console.log("express server on port", port)
