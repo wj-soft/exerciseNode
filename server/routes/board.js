167f9c4043262ddaadf435070864ee5bb63021a4
wj-soft <wooju_lee@daum.net>
2018-03-27 18:23:54 +0900

    edit & delete

--- server/routes/board.js
+++ server/routes/board.js
@@ -8,22 +8,40 @@ router.get('/', function (req, res) {
 });
 
 router.get('/write', function (req, res) {
-  res.render('addContent');
+  res.render('addContent', {contents:""});
 });
 
 router.post('/write', function (req, res) {
   var notice = new NoticeModel({
     title: req.body.title,
-    contents: req.body.content,
+    contents: req.body.content
   });
   notice.save(function (err) {
     res.redirect('/');
   });
 });
 
-router.get('/delete/:id', function (req, res) {
+router.get('/delete/:pk', function (req, res) {
   NoticeModel.remove({ pk: req.params.id }, function (err) {
     res.redirect('/');
   });
 });
+
+router.get('/edit/:pk', function (req, res) {
+  NoticeModel.findOne({ pk: req.params.pk }, function (err, contents) {
+    res.render('addContent', { contents: contents });
+  });
+});
+
+router.post('/edit/:pk', function (req, res) {
+  var query = {
+    title: req.body.title,
+    contents: req.body.content
+  };
+
+  NoticeModel.update({ pk: req.params.pk }, { $set: query }, function (err) {
+    res.redirect('/'); 
+  });
+});
+
 module.exports = router;
